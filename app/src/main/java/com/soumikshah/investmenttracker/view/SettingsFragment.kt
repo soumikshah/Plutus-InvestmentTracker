3e41323dd776c641aba3b3472159c72815ed7f3c
Soumik <soumik@hotelwifi.com>
2021-07-17 14:18:41 +0530

    added a placeholder for settings fragment

--- app/src/main/java/com/soumikshah/investmenttracker/view/SettingsFragment.kt
+++ app/src/main/java/com/soumikshah/investmenttracker/view/SettingsFragment.kt
@@ -1,5 +1,20 @@
 package com.soumikshah.investmenttracker.view
 
+import android.os.Bundle
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
+import android.widget.TextView
 import androidx.fragment.app.Fragment
+import com.soumikshah.investmenttracker.R
 
-class SettingsFragment internal constructor() : Fragment()
+class SettingsFragment internal constructor() : Fragment(){
+    var sampleTextView:TextView? =null
+    override fun onCreateView(inflater: LayoutInflater, container: ViewGroup?, savedInstanceState: Bundle?): View? {
+        val view = inflater.inflate(R.layout.fragment_settings, container, false)
+        sampleTextView = view.findViewById(R.id.sampleTextView)
+        sampleTextView!!.text = getString(R.string.sampletext)
+        return view
+    }
+
+}
